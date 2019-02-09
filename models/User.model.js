d96ab576752ef64949e17f35bbb2d375c12088fd
BuildTools <unconfigured@null.spigotmc.org>
2019-02-09 22:11:31 +0200

    added user model

---
+++ models/User.model.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({,
+    _id: {
+        type: String
+    },
+    firstName: {
+        type: String,
+        required: true,
+        minLength: 1,
+        maxLength: 100
+    },
+    lastName: {
+        type: String,
+        required: true,
+        minLength: 1,
+        maxLength: 100
+    },
+    email: {
+        type: String,
+        required: true,
+        minLength: 1,
+        match: /^.+@.+\..+$/
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 8,
+        minLength: 24,
+    }
+});
+
+module.exports = mongoose.model("User", userSchema);
