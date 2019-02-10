fe67c3f40dfae17c7090894ba24123c2d40c5ac1
BuildTools <unconfigured@null.spigotmc.org>
2019-02-11 00:19:27 +0200

    post to users and token helpers

--- index.js
+++ index.js
@@ -5,15 +5,10 @@ const bodyParser = require("body-parser");
 const users = require("./views/users");
 
 const app = express();
+app.use(bodyParser.json());
 
-<<<<<<< HEAD
-app.get("/", (req, res) => {
-    res.send("Hello world!");
-});
-=======
 mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true});
 
 app.all("/api/users", users);
->>>>>>> a6e321778e46964e27a676f56ff4305fd80b4ccb
 
 app.listen(3000, () => console.log("Server listening on port 3000"));
--- views/users.js
+++ views/users.js
@@ -1,4 +1,7 @@
 const errors = require("./errors");
+const stringers = require("../misc/stringers");
+const config = require("../misc/config");
+const crypto = require("crypto");
 
 const userModel = require("../models/User.model");
 
@@ -29,7 +32,32 @@ const getUsers = (req, res) => {
 }
 
 const postUsers = (req, res) => {
-    res.send("POST!");
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const email = req.body.email;
+    const salt = stringers.generateRandomString(5);
+    const password = req.body.password.length > 0 && typeof(req.body.password) == "string" ? crypto.createHash("sha512").update(req.body.password + salt).digest("base64") : "";
+
+    var newUser = new userModel({
+        firstName,
+        lastName,
+        email,
+        password,
+        salt
+    });
+
+    newUser.save((err, user) => {
+        if (err) {
+            if (typeof(err.errors) == "undefined") {
+                errors.emailTaken(req, res);
+            } else {
+                errors.invalidArguments(req, res, Object.keys(err.errors));
+            }
+        } else {
+            res.status(201);
+            res.json(user);
+        }
+    })
 }
 
 module.exports = users;
