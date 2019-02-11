ce0d1d334468b7e1c73dbaae4e007383c2ddcd90
BuildTools <unconfigured@null.spigotmc.org>
2019-02-11 17:44:57 +0200

    add auth

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
 const users = require("./views/users");
+const tokens = require("./views/tokens");
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,5 +11,6 @@ app.use(bodyParser.json());
 mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true});
 
 app.all("/api/users", users);
+app.all("/api/tokens", tokens);
 
 app.listen(3000, () => console.log("Server listening on port 3000"));
---
+++ models/Token.model.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const tokenSchema = new Schema({
+    _id: String
+});
+
+module.exports = mongoose.model("Token", tokenSchema);
