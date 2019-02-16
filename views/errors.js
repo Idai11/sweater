007baf1cc1cab256b6fba6e919d8ace6e4072ee6
BuildTools <unconfigured@null.spigotmc.org>
2019-02-16 14:11:52 +0200

    quality of life changes for chervin ❤️

--- views/errors.js
+++ views/errors.js
@@ -5,7 +5,9 @@ const errors = {};
 errors.methodNotAllowed = (req, res) => {
     res.status(400);
     res.json({
-        "message": "Method not allowed"
+        "error": true,
+        "errorCode": 1,
+        "errorMessage": "Method not allowed"
     });
 }
 
@@ -16,14 +18,18 @@ errors.databaseError = (req, res, err) => {
 
     res.status(500);
     res.json({
-        "message": "Database error"
+        "error": true,
+        "errorCode": 2,
+        "errorMessage": "Database error"
     });
 }
 
 errors.invalidArguments = (req, res, invalid) => {
     res.status(400);
     res.json({
-        "message": "Invalid arguments",
+        "error": true,
+        "errorCode": 3,
+        "errorMessage": "Invalid arguments",
         invalid
     });
 }
@@ -31,28 +37,36 @@ errors.invalidArguments = (req, res, invalid) => {
 errors.emailTaken = (req, res) => {
     res.status(400);
     res.json({
-        "message": "Email address is already taken"
+        "error": true,
+        "errorCode": 4,
+        "errorMessage": "Email address is already taken"
     });
 }
 
 errors.loginFailed = (req, res) => {
     res.status(401);
     res.json({
-        "message": "Authentication failed"
+        "error": true,
+        "errorCode": 5,
+        "errorMessage": "Authentication failed"
     });
 }
 
 errors.unauthorized = (req, res) => {
     res.status(401);
     res.json({
-        "message": "You are not authorized to perform this action"
+        "error": true,
+        "errorCode": 6,
+        "errorMessage": "You are not authorized to perform this action"
     });
 }
 
 errors.notFound = (req, res, name) => {
     res.status(404);
     res.json({
-        "message": `${name} not found`
+        "error": true,
+        "errorCode": 7,
+        "errorMessage": `${name} not found`
     });
 }
 
