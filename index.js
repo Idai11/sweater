c0b24c132dc8cec7584b48234080d17e5d016868
BuildTools <unconfigured@null.spigotmc.org>
2019-02-19 22:25:45 +0200

    removing pot views

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ const config = require("./misc/config");
 const users = require("./views/users");
 const user = require("./views/user");
 const tokens = require("./views/tokens");
-const pots = require("./views/pots");
-const pot = require("./views/pot");
 
 const app = express();
 app.use(bodyParser.json());
@@ -18,7 +16,5 @@ mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true});
 app.all("/api/users", users);
 app.all("/api/users/:userId", user);
 app.all("/api/tokens", tokens);
-app.all("/api/pots", pots);
-app.all("/api/pots/:potId", pot);
 
 app.listen(config.port, () => console.log("Server listening on port 3000"));
