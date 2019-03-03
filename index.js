71a6e85fddd3e929f03ae772ecbd4dee81b8f33d
BuildTools <unconfigured@null.spigotmc.org>
2019-03-03 16:02:50 +0200

    404 handling and request logging

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const config = require("./misc/config");
 const users = require("./views/users");
 const user = require("./views/user");
 const tokens = require("./views/tokens");
+const errors = require("./views/errors");
 
 const app = express();
 // Setup the JSON request parser
@@ -17,10 +18,22 @@ app.use(express.static("static"));
 // Connect to test database
 mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true});
 
+// Request logging
+app.use((req, res, next) => {
+    console.log(`${req.method} request to ${req.url}`);
+    next();
+});
+
 // URL Map
 app.all("/api/users", users);
 app.all("/api/users/:userId", user);
 app.all("/api/tokens", tokens);
+
+// Catch 404s
+app.use((req, res, next) => {
+  res.status(404);
+  errors.notFound(req, res, req.url);
+})
 
 // Listen on port 3000
 app.listen(config.port, () => console.log("Server listening on port 3000"));
