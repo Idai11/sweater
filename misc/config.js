54696a1acd275db22527015b648ef2cb7e743378
BuildTools <unconfigured@null.spigotmc.org>
2019-12-31 01:58:20 +0200

    heroku port fix

--- misc/config.js
+++ misc/config.js
@@ -1,7 +1,7 @@
 const config = {
     hashKey: "Cg{0`)2s>(nDU/&.}|19oh<y", // The secret for hashing passwords
     tokenKey: "]h:ElLE_yP60BP%UK$^w<>:^", // The secret for signing auth tokens
-    port: 3000, // The port the server runs on
+    port: process.env.PORT, // The port the server runs on
     debug: true // Is the app running in debug?
 };
 
