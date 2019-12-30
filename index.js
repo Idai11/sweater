43a602694f3e363f28cf0bbd4d1095a866ee75d5
BuildTools <unconfigured@null.spigotmc.org>
2019-12-31 01:44:51 +0200

    log database uri

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.use(cors());
 app.use(express.static("static"));
 
 // Connect to test database
+console.log(process.env.MONGODB_URI)
 mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true});
 
 // Pre-Request middleware
